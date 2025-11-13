edd98307a0bbe7cdf32283cf47c8dfb37c3b752e
Jagapathi-Vallapuri <jagapathivallapuri444@gmail.com>
2025-11-13 22:55:34 +0530

    migrations and supabase connections

---
+++ app/db/database.py
@@ -0,0 +1,32 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+USER = os.getenv("user")
+PASSWORD = os.getenv("password")
+HOST = os.getenv("host")
+PORT = os.getenv("port")
+DBNAME = os.getenv("dbname")
+
+DATABASE_URL = f"postgresql+psycopg://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
+
+engine = create_async_engine(
+    DATABASE_URL,
+    echo = False,
+    pool_size=10,
+    max_overflow=20
+)
+
+async_session = async_sessionmaker(
+    autocommit = False,
+    autoflush=False,
+    bind=engine,
+    class_=AsyncSession,
+)
+
+
+async def get_db():
+    async with async_session() as session:
+        yield session
