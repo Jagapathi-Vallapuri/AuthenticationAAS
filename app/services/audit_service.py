49c8eb5612a275dec84081d961a232aabc560b97
Jagapathi-Vallapuri <jagapathivallapuri444@gmail.com>
2025-11-14 18:40:06 +0530

    servicess

---
+++ app/services/audit_service.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+from typing import Optional, Dict, Any
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.models.AuditLog import AuditLog
+from app.models.AuditAction import AuditAction
+
+async def log_section(
+        db: AsyncSession, 
+        user_id:Optional[int], 
+        action_type: AuditAction, 
+        metadata: Optional[Dict[str, Any]] = None,
+        ip_address: Optional[str] = None,
+        user_agent: Optional[str] = None
+        ) -> None:
+    try:
+        log = AuditLog(
+            user_id=user_id,
+            action_type = action_type,
+            metadata=metadata or {},
+            ip_address= ip_address,
+            user_agent=user_agent
+        )
+
+        db.add(log)
+        await db.flush()
+        
+    except Exception:
+        pass
